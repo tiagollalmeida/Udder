c45c0dbe4d7f53061a59fa0956aababa232e6f79
Tiago Almeida <tiagollalmeida@hotmail.com>
2020-05-08 22:11:07 -0500

    First commit

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+                VStack {
+            VStack {
+                Text("UDDER!")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Vidalokkca")
+            }
+            Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+        }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
