73c0bb24008fb859e49ced8a3ca881e766cd1a7a
Tiago Almeida <tiagollalmeida@hotmail.com>
2020-05-08 21:48:52 -0500

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Tiago on 08/05/20.
+//  Copyright © 2020 caribbeanx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
